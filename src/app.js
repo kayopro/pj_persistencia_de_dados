68069d2315d8ec2abd9afcf65739e996da51aec8
José Kayo <kayokj@gmail.com>
2024-06-28 14:00:27 -0300

    pj persistencia de dados

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/userRouter");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Conectando ao banco de dados MongoDB
+mongoose.connect("mongodb+srv://usarApp:s4ads123@aulaweb.aefv2ko.mongodb.net", {
+  retryWrites: true,
+  w: "majority",
+  appName: "AulaWeb",
+});
+
+app.use("/api", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
